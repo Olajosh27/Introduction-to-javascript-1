082a27f5532ccd0bb7b7b68b57566675ed878779
Olaniyi Olanrewaju <96597292+Olajosh27@users.noreply.github.com>
2022-06-23 00:25:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var firstName ='Olanrewaju';
+var middleName ='Joshua';
+var lastName ='Olaniyi';
+var height ='1.86m';
+var country ='Nigeria';
+
+name = firstName+' '+middleName+' '+lastName;
+
+alert('Name:'+' '+ name );
+alert('Height:'+' '+ height );
+alert('Country:'+' '+ country );
+alert('My name is'+' '+name+' ,'+'  ' +'i am'+ height+ ' '+'tall'+ ' '+'and i am from'+' '+country)
+
